b47e5ff948789ab9bb063c44a19a65106cae0fc9
Pranjal Chaplot <pranjalchaplot@gmail.com>
2023-01-16 15:41:54 +0530

    add empty tx list image holder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,11 +10,30 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 450,
-      child: ListView.builder(
-        itemBuilder: ((context, index) =>
-            TransactionCard(_userTransaction[index])),
-        itemCount: _userTransaction.length,
-      ),
+      child: _userTransaction.isEmpty
+          ? Column(
+              children: [
+                const Text(
+                  'No Transaction Available',
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  //wrapped in container because img parent(column takes infinite height)
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: ((context, index) =>
+                  TransactionCard(_userTransaction[index])),
+              itemCount: _userTransaction.length,
+            ),
     );
   }
 }
